a63a1b7bd14fa93a7e96b230b65453db05700797
Kator James <kator95@gmail.com>
2017-03-16 07:02:46 +0000

    m

--- app/Http/Controllers/Front.php
+++ app/Http/Controllers/Front.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Validator;
+
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
