d900e8a9ad27687aabbd1e8f026be6f13bd36aff
DR <d.retnan@nhubnigeria.com>
2017-03-22 13:08:19 +0000

    m

--- resources/views/plain.blade.php
+++ resources/views/plain.blade.php
@@ -21,10 +21,10 @@
       @yield('content')
       </div>
     </div>
-    <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
-    <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
-    <script src="assets/js/main.js" type="text/javascript"></script>
-    <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="/assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
+    <script src="/assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+    <script src="/assets/js/main.js" type="text/javascript"></script>
+    <script src="/assets/lib/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
     <script type="text/javascript">
       $(document).ready(function(){
         //initialize the javascript
