f1da6faf7983d1314a077a4e063aa6fca11d9f3c
DR <d.retnan@nhubnigeria.com>
2017-03-22 12:44:00 +0000

    m

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
     <div class="splash-container">
       <div class="panel panel-default panel-border-color panel-border-color-primary">
         <div class="panel-heading">
-          <a href="/"><img src="/assets/img/grandpayer-admin.png" alt="{{ config('settings.app_name') }}" width="160" height="70" class="logo-img"></a>
+          <a href="/"><img src="assets/img/grandpayer-admin.png" alt="{{ config('settings.app_name') }}" width="160" height="70" class="logo-img"></a>
           <span class="splash-description">Please enter your user information.</span>
           @if (count($errors))
               @foreach($errors as $error) 
