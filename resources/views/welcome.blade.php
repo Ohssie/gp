5a7bf0d60bb245e20227cbb7141f2bf89a6c2cd5
DR <d.retnan@nhubnigeria.com>
2017-03-27 19:20:25 +0000

    m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,15 +16,15 @@
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400%7CRaleway:300,400,500,600,700%7CLato:300,400,400italic,600,700" rel="stylesheet" type="text/css" />
 
 		<!-- CORE CSS -->
-		<link href="assets2/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="/assets2/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 
 		<!-- THEME CSS -->
-		<link href="assets2/css/essentials.css" rel="stylesheet" type="text/css" />
-		<link href="assets2/css/layout.css" rel="stylesheet" type="text/css" />
+		<link href="/assets2/css/essentials.css" rel="stylesheet" type="text/css" />
+		<link href="/assets2/css/layout.css" rel="stylesheet" type="text/css" />
 
 		<!-- PAGE LEVEL SCRIPTS -->
-		<link href="assets2/css/header-1.css" rel="stylesheet" type="text/css" />
-		<link href="assets2/css/layout-shop.css" rel="stylesheet" type="text/css" />
+		<link href="/assets2/css/header-1.css" rel="stylesheet" type="text/css" />
+		<link href="/assets2/css/layout-shop.css" rel="stylesheet" type="text/css" />
 		
 		<style>
 			img.img-responsive.radius-4{
@@ -51,7 +51,7 @@
                 background: linear-gradient(to bottom, #555555 0%,#21191b 100%);
             }
 		</style>
-		<link rel="stylesheet" href="assets2/layerslider.css">
+		<link rel="stylesheet" href="/assets2/layerslider.css">
 	</head>
 	<body class="smoothscroll enable-animation">
 		<!-- wrapper -->
@@ -287,7 +287,7 @@
                             layersContainer: 	1280,
 
                             skin:				'borderlessdark',
-                            skinsPath: 			"assets2/plugins/slider.layerslider/skins/"
+                            skinsPath: 			"/assets2/plugins/slider.layerslider/skins/"
                         }
 					</script>
 
@@ -449,7 +449,7 @@
 						<li class="col-md-4">
 							<div class="testimonial">
 								<figure class="pull-left">
-									<img class="rounded" src="assets2/girl.png" alt="" />
+									<img class="rounded" src="/assets2/girl.png" alt="" />
 								</figure>
 								<div class="testimonial-content">
 									<p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
@@ -463,7 +463,7 @@
 						<li class="col-md-4">
 							<div class="testimonial">
 								<figure class="pull-left">
-									<img class="rounded" src="assets2/girl.png" alt="" />
+									<img class="rounded" src="/assets2/girl.png" alt="" />
 								</figure>
 								<div class="testimonial-content">
 									<p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
@@ -505,7 +505,7 @@
 						<li class="col-md-4">
 							<div class="testimonial">
 								<figure class="pull-left">
-									<img class="rounded" src="assets2/girl.png" alt="" />
+									<img class="rounded" src="/assets2/girl.png" alt="" />
 								</figure>
 								<div class="testimonial-content">
 									<p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
@@ -644,7 +644,7 @@
 		<a href="#" id="toTop"></a>
 
 
-		<!-- PRELOADER -->
+		 <!--PRELOADER -->
 		<div id="preloader">
 			<div class="inner">
 				<span class="loader"></span>
@@ -653,16 +653,16 @@
 
 
 		<!-- JAVASCRIPT FILES -->
-		<script type="text/javascript">var plugin_path = "assets2/plugins/";</script>
-		<script type="text/javascript" src="assets2/plugins/jquery/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript">var plugin_path = "/assets2/plugins/";</script>
+		<script type="text/javascript" src="/assets2/plugins/jquery/jquery-2.1.4.min.js"></script>
 
-		<script type="text/javascript" src="assets2/js/scripts.js"></script>
+		<script type="text/javascript" src="/assets2/js/scripts.js"></script>
 
 		<!-- STYLESWITCHER - REMOVE -->
 		<!--<script async type="text/javascript" src="{{ url('assets2/plugins/styleswitcher/styleswitcher.js') }}"></script>-->
 
 		<!-- PAGE LEVEL SCRIPTS -->
-		<script type="text/javascript" src="assets2/js/view/demo.shop.js"></script>
+		<script type="text/javascript" src="/assets2/js/view/demo.shop.js"></script>
 		<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
@@ -748,7 +748,7 @@
 
 
 
-		<script src="assets2/layerslider_pack.js"></script>
-		<script src="assets2/demo.layerslider_slider.js"></script>
+		<script src="/assets2/layerslider_pack.js"></script>
+		<script src="/assets2/demo.layerslider_slider.js"></script>
 	</body>
 </html>
