94c800e247b0cc6a55a10adb9d06c88719a1db91
DR <d.retnan@nhubnigeria.com>
2017-04-01 22:31:41 +0000

    m

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -11,6 +11,6 @@ class AdminUserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(['name' => 'Ayesha Danjuma', 'username' => 'ifendi17', 'password' => bcrypt('*ton@7475'), 'phone' => '07062193996', 'bank_name' => 'Guaranty Trust Bank', 'account_name' => 'Ayesha Fred', 'account_number' => '0026058761', 'role' => 'admin']);
+        DB::table('users')->insert(['name' => 'Ayesha Danjuma', 'username' => 'ifendi17', 'password' => bcrypt('*ton@7475'), 'phone' => '08062856202', 'bank_name' => 'Guaranty Trust Bank', 'account_name' => 'Ayesha Fred', 'account_number' => '0026058761', 'role' => 'admin']);
     }
 }
