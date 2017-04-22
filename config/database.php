4212bd4b22aa53473cb07578f6ee8ed79f4a15b0
DR <d.retnan@nhubnigeria.com>
2017-04-22 16:42:28 +0000

    m

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'citisumo'),
+            'database' => env('DB_DATABASE', 'cititest'),
             'username' => env('DB_USERNAME', 'citisumo'),
             'password' => env('DB_PASSWORD', 'citisumo'),
             'charset' => 'utf8',
@@ -65,6 +65,20 @@ return [
             'strict' => false,
             'engine' => null,
         ],
+        
+        /*'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'citisumo'),
+            'username' => env('DB_USERNAME', 'citisumo'),
+            'password' => env('DB_PASSWORD', 'citisumo'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
